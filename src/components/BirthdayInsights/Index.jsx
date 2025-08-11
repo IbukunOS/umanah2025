932f0694bdba925acfa91cee76d6475f2f74d18f
Ibukun-Odunsi Semiloore <109909221+Floopydisk@users.noreply.github.com>
2025-08-11 13:59:20 +0100

    wahala for family drama oo

--- src/components/BirthdayInsights/Index.jsx
+++ src/components/BirthdayInsights/Index.jsx
@@ -1,4 +1,4 @@
-import img1 from '../../assets/images/ejiro.png';
+import img1 from '../../assets/images/eno.png';
 import { useEffect } from 'react';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -37,19 +37,18 @@ function BirthdayInsights() {
             <div className='hidden w-[50%] sm:flex items-center justify-center mb-12'>
                 <hr className='bg-zinc-400 w-[40%] h-[.3vh]' />
             </div>    
-            <h3 className='textpara sm:w-[70%] text-blue-600 font-[Sansita] tracking-wide text-[2.4vh] sm:text-[3.5vh] font-medium text-center leading-[5vh] mb-10'>To the woman who stepped up when no one else did, thank you for being both our mum and dad.  
-They say a mother's love is powerful, but a mother who fathers is unstoppable. You carried the weight of two roles, yet never let us feel the lack of one.
-
-Well, it's your birthday, Mum...  
-And also, your official retirement. I know you always tell us how proud you are, but I am the proud one here.  
-Thank you for being the best. I literally don't even have a best friend because my mum is always there for me.  
-Words are not enough to express how grateful I am.  
-Thank you, Mumsy.</h3>
+            <h3 className='textpara sm:w-[70%] text-blue-600 font-[Sansita] tracking-wide text-[2.4vh] sm:text-[3.5vh] font-medium text-center leading-[5vh] mb-10'>
+                Today we celebrate an extraordinary milestone for an extraordinary woman — my mom. Not only is she turning 60 today, but she’s also stepping into a well-earned retirement after a lifetime of impact, service, and love.
+                She has always been more than just a mother to me and to many others. She is a woman of God, a leader, a guardian, a mentor, and a true friend. When she sees a problem, she never looks away. She does her best to solve it, and even if she can’t fix it all, she makes sure you never face it alone.
+                After my dad passed away, she carried the roles of both mother and father without complaint, showing strength and grace beyond measure. Even though I’m an only child, I never felt alone — a true testament to her love and the home she created.
+                Her kindness is magnetic, her intentions pure, and people are naturally drawn to her because they know she is genuine.
+                Happy 60th birthday and congratulations on your retirement, Mom. You’ve lived this chapter with courage and compassion, and I can’t wait to see what this next one holds for you.
+                </h3>
             <div className="pers w-[50%] flex flex-col items-center justify-center gap-2 ">
                 <div className="image w-24 h-24 overflow-hidden rounded-full ">
                     <img src={img1} />
                 </div>
-                <h1 className=' text-[2.8vh] sm:text-[3.8vh] font-medium'>Onakorame E. Esther</h1>
+                <h1 className=' text-[2.8vh] sm:text-[3.8vh] font-medium'>Umanah Eno Abasi</h1>
                 <h3 className='text-zinc-500 text-[2.4vh] whitespace-nowrap'>Celebrant Pikin</h3>
             </div>
         </div>  
